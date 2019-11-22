9d4e7d452f40b0d9279dbc0f53e0237543906db9
eduardo.reichert <eduardo.reichert@wssim.com.br>
2019-11-22 13:39:05 -0300

    Adding persistor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import './config/RectotronConfig';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './services/history';
-import store from './store';
-
-// import { Container } from './styles';
+import { store, persistor } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
